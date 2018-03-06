a44b6654ecfa5dc541bfd81e8b7730b82f1d608e
Melanie Phillips <melanieleeking@gmail.com>
2018-03-06 16:27:58 -0500

    Added styling for login, added images/icons, added topbar styling.

--- dev/scripts/TopBar.js
+++ dev/scripts/TopBar.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
-
 class TopBar extends React.Component {
 
     render() {
         return(
-            <div>
-                <div>logo</div>
-                <div>Username</div>
-                <div>
-                    <Link to={"/"}>
-                        <button>Home</button>
-                    </Link>
-                    <Link to={"./WatchList"}>
-                        <button>List</button>
-                    </Link>
-                </div>
+            <div className="topbar__div clearfix">
+                <div className="topbar__div__logodiv"><img src="/dev/images/icon_logo_white.png" alt=""/></div>
+                <h4>Username Username</h4>
+                    <div className="topbar__div__navbar__icondiv button_home">
+                        <Link to={"/"}>
+                            <img src="/dev/images/icon_home_pink.png" alt="" />
+                        </Link>
+                    </div>
+                    <div className="topbar__div__navbar__icondiv button_list">
+                        <Link to={"/WatchList"}>
+                            <img src="/dev/images/icon_list_pink.png" alt="" />
+                        </Link>
+                    </div>
             </div>
         )
     }
 }
-
-
 
 export default TopBar;
