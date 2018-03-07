13396e5dbb86e4b62c6703e2f8316431876beec0
Melanie Phillips <melanieleeking@gmail.com>
2018-03-06 19:49:08 -0500

    Styling for logged in page.  Image changes.

--- dev/scripts/SearchGenre.js
+++ dev/scripts/SearchGenre.js
@@ -12,33 +12,34 @@ class SearchGenre extends React.Component {
         console.log(this.props);
 
         return (
-            <div>
-                <form>
-                    <select onChange={this.props.passState} >
-                        {/* {this.props.genreName} */}
-                        return(
-                            {this.props.genres.map((genre, i) => {
-                            return (
-                                <option key={i} value={genre.id}>{genre.name}</option>
+            <div className="search__genrediv">
+                <h4>Search By Genre:</h4>
+                <form className="searchform genre clearfix">
+                        <select className="clearfix" onChange={this.props.passState} >
+                            {/* {this.props.genreName} */}
+                            return(
+                                {this.props.genres.map((genre, i) => {
+                                return (
+                                    <option key={i} value={genre.id}>{genre.name}</option>
+                                )
+                            })}
                             )
-                        })}
-                        )
-                    </select>
-                </form>
-                <Link to={
-                    {
-                        pathname: '/SearchResultsGenre',
+                        </select>                        
+                    <Link className="clearfix" to={
+                        {
+                            pathname: '/SearchResultsGenre',
 
-                        state: { 
-                            name: this.props.genreRequest,
-                            user: this.props.user,
-                            username: this.props.username }
-                    }}>
-                    {/* <input type="submit" /> */}
-                    
-                    <button>Submit small change</button>
-                    
-                </Link>
+                            state: { 
+                                name: this.props.genreRequest,
+                                user: this.props.user,
+                                username: this.props.username }
+                        }}>
+                        {/* <input type="submit" /> */}
+                        
+                        <button className="search_button searchicon_genre"><img src="/dev/images/icon_search.png" alt="" /></button>
+                        
+                    </Link>
+                </form>
             </div>
         )
     }
